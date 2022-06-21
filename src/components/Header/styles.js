f18b4f60cd9d9183589fb6fc1da1477f0065bcd4
Amanda Bartolo <amanda.bartolo@somagrupo.com.br>
2022-06-21 12:06:08 -0300

    Projeto completo (v1)

---
+++ src/components/Header/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div
+    `height: 150px;
+    text-align: center;
+    background: teal;`
+
+export const Header = styled.h1``
+
+export const Title = styled.div
+    `padding-top: 20px;
+    color: #fff;
+    text-shadow: 1px 1px 1px #fff`
